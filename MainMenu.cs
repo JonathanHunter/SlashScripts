c0fa5d20ce0c5087b2b4091febef18ae42637878
Jonathan Hunter <hunterjonathant@gmail.com>
2014-05-24 21:18:04 -0400

    added GUI

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+﻿//written by: Jonathan Hunter
+using UnityEngine;
 using System.Collections;
 namespace Assets.Scripts
 {
     public class MainMenu : MonoBehaviour
     {
+        public Texture Title, CursorPic;
         public GameObject LevelSelect, Video, Audio, Controls;
-        private enum State { Main, LevelSelect, Options, Credits, Video, Audio, Controls };
+        public GUIStyle ButtonStyle;
+        private enum State { Main = 0, LevelSelect, Options, Credits, Video, Audio, Controls, Exit };
         private int cursor;
         private State currState;
         private State nextState;
@@ -57,7 +60,7 @@ namespace Assets.Scripts
             {
                 if (CustomInput.Down)
                 {
-                    if (cursor == (int)State.Controls)
+                    if (cursor == (int)State.Exit)
                         cursor = (int)State.Video;
                     else
                         cursor++;
@@ -65,11 +68,11 @@ namespace Assets.Scripts
                 if (CustomInput.Up)
                 {
                     if (cursor == (int)State.Video)
-                        cursor = (int)State.Controls;
+                        cursor = (int)State.Exit;
                     else
                         cursor--;
                 }
-                if (CustomInput.Cancel)
+                if ((cursor == (int)State.Exit && CustomInput.Accept)||CustomInput.Cancel)
                     nextState = State.Main;
                 if (cursor == (int)State.Video && CustomInput.Accept)
                 {
@@ -111,19 +114,49 @@ namespace Assets.Scripts
                 nextState = State.Main;
             currState = nextState;
         }
-        void OnGUI()
+        void OnGUI()//values based off of 19x12 grid
         {
             if (currState == State.Main)
             {
-
+                //left, top, width, height
+                //title pic
+                GUI.DrawTexture(new Rect(Screen.width * (6f / 19f), Screen.height * (1f / 12f), Screen.width * (7f / 19f), Screen.height * (2f / 12f)), Title);
+                //cursor
+                GUI.DrawTexture(new Rect(Screen.width * (7f / 19f), Screen.height * ((5f / 12f) + ((cursor - (int)State.LevelSelect / 12) * 2)), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
+                //menu buttons
+                if (GUI.Button(new Rect(Screen.width * (8f / 19f), Screen.height * (5f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Play", ButtonStyle))
+                    nextState = State.LevelSelect;
+                if (GUI.Button(new Rect(Screen.width * (8f / 19f), Screen.height * (7f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Options", ButtonStyle))
+                    nextState = State.Options;
+                if (GUI.Button(new Rect(Screen.width * (8f / 19f), Screen.height * (9f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Credits", ButtonStyle))
+                    nextState = State.Credits;
             }
             if (currState == State.Options)
             {
+                //left, top, width, height
+                //title pic
+                GUI.DrawTexture(new Rect(Screen.width * (6f / 19f), Screen.height * (1f / 12f), Screen.width * (7f / 19f), Screen.height * (2f / 12f)), Title);
+                //Label denoting that this is the options menu
+                GUI.Label(new Rect(Screen.width * (7f / 19f), Screen.height * (4f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Options", ButtonStyle);
+                //cursor
+                if (cursor == (int)State.Exit)
+                    GUI.DrawTexture(new Rect(Screen.width * (6f / 19f), Screen.height * (10f / 12f), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
+                else
+                    GUI.DrawTexture(new Rect(Screen.width * (6f / 19f), Screen.height * ((6f / 12f) + ((cursor - (int)State.Video / 12))), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
+                //menu buttons
+                if (GUI.Button(new Rect(Screen.width * (7f / 19f), Screen.height * (6f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Video", ButtonStyle))
+                    nextState = State.Video;
+                if (GUI.Button(new Rect(Screen.width * (7f / 19f), Screen.height * (7f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Audio", ButtonStyle))
+                    nextState = State.Audio;
+                if (GUI.Button(new Rect(Screen.width * (7f / 19f), Screen.height * (8f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Controls", ButtonStyle))
+                    nextState = State.Controls;
+                if (GUI.Button(new Rect(Screen.width * (7f / 19f), Screen.height * (10f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Exit", ButtonStyle))
+                    nextState = State.Main;
 
             }
             if (currState == State.Credits)
             {
-
+                //pic or text box
             }
         }
     }
--- Video.cs
+++ Video.cs
@@ -1,22 +1,91 @@
-﻿using UnityEngine;
+﻿//written by: Jonathan Hunter
+using UnityEngine;
 using System.Collections;
 namespace Assets.Scripts
 {
     class Video : MonoBehaviour
     {
-        public GUIStyle style;
+        public string VideoKey;//0:resIndex, 1:fullscreen(1=on), 2:quality
+        public Texture Title, CursorPic;
+        public GUIStyle LabelStyle;
+        private enum State { Resolution = 0, FullScreen, Quality, Accept, Cancel }
+        private int cursor, quality, resIndex;
+        private bool fullscreen, a = true;
         void Start()
         {
-
+            cursor = (int)State.Resolution;
+            resIndex = PlayerPrefs.GetInt(VideoKey + 0);
+            fullscreen = Screen.fullScreen;
+            quality = QualitySettings.GetQualityLevel();
         }
         void Update()
         {
-            if (CustomInput.Cancel)
+            if (CustomInput.Cancel || (cursor == (int)State.Cancel && CustomInput.Accept))
                 Destroy(this.gameObject);
+            if (CustomInput.Down)
+            {
+                if (cursor == (int)State.Cancel)
+                    cursor = (int)State.Resolution;
+                else
+                    cursor++;
+            }
+            if (CustomInput.Up)
+            {
+                if (cursor == (int)State.Resolution)
+                    cursor = (int)State.Cancel;
+                else
+                    cursor--;
+            }
+            if (cursor == (int)State.Resolution && CustomInput.Accept)
+            {
+                resIndex++;
+                if (resIndex >= Screen.resolutions.Length)
+                    resIndex = 0;
+            }
+            if (cursor == (int)State.FullScreen && CustomInput.Accept)
+                fullscreen = !fullscreen;
+            if (cursor == (int)State.Accept && CustomInput.Accept)
+                Accept();
         }
-        void OnGUI()
+        private void Accept()
         {
-            GUI.Box(new Rect(Screen.width / 3f, Screen.height / 3, Screen.width / 2, Screen.height / 2), "Video", style);
+            Screen.SetResolution(Screen.resolutions[resIndex].width, Screen.resolutions[resIndex].height, fullscreen);
+            QualitySettings.SetQualityLevel(quality);
+            Destroy(this.gameObject);
+        }
+        void OnGUI()//values based off of 19x12 grid
+        {
+            //left, top, width, height
+            //title pic
+            GUI.DrawTexture(new Rect(Screen.width * (6f / 19f), Screen.height * (1f / 12f), Screen.width * (7f / 19f), Screen.height * (2f / 12f)), Title);
+            //menu title
+            GUI.Label(new Rect(Screen.width * (7f / 19f), Screen.height * (3f / 12f), Screen.width * (4f / 19f), Screen.height * (1f / 12f)), "Video", LabelStyle);
+            //Resolution
+            GUI.Label(new Rect(Screen.width * (6f / 19f), Screen.height * (4.5f / 12f), Screen.width * (3f / 19f), Screen.height * (1f / 12f)), "Resolution", LabelStyle);
+            Popup.List(new Rect(Screen.width * (10f / 19f), Screen.height * (4.5f / 12f), Screen.width * (3f / 19f), Screen.height * (1f / 12f)), ref a, ref resIndex, new GUIContent(Screen.resolutions[resIndex].ToString()), Screen.resolutions, LabelStyle);
+            //Fullscreen
+            GUI.Label(new Rect(Screen.width * (6f / 19f), Screen.height * (6f / 12f), Screen.width * (3f / 19f), Screen.height * (1f / 12f)), "Fullscreen", LabelStyle);
+            fullscreen = GUI.Toggle(new Rect(Screen.width * (11f / 19f), Screen.height * (6f / 12f), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), fullscreen, fullscreen ? "ON" : "OFF");
+            //Quality
+            GUI.Label(new Rect(Screen.width * (6f / 19f), Screen.height * (7.5f / 12f), Screen.width * (3f / 19f), Screen.height * (1f / 12f)), "Quality", LabelStyle);
+            Popup.List(new Rect(Screen.width * (10f / 19f), Screen.height * (7.5f / 12f), Screen.width * (3f / 19f), Screen.height * (1f / 12f)), ref a, ref quality, new GUIContent(QualitySettings.names[quality].ToString()), QualitySettings.names, LabelStyle);
+            //Accept
+            if (GUI.Button(new Rect(Screen.width * (6f / 19f), Screen.height * (9.5f / 12f), Screen.width * (3f / 19f), Screen.height * (1f / 12f)), "Accept", LabelStyle))
+                Accept();
+            //Cancel
+            if (GUI.Button(new Rect(Screen.width * (10f / 19f), Screen.height * (9.5f / 12f), Screen.width * (3f / 19f), Screen.height * (1f / 12f)), "Cancel", LabelStyle))
+                Destroy(this.gameObject);
+            //cursor
+            if (cursor == (int)State.Resolution)
+                GUI.DrawTexture(new Rect(Screen.width * (5f / 19f), Screen.height * (4.5f / 12f), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
+            else if (cursor == (int)State.FullScreen)
+                GUI.DrawTexture(new Rect(Screen.width * (5f / 19f), Screen.height * (6f / 12f), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
+            else if (cursor == (int)State.Quality)
+                GUI.DrawTexture(new Rect(Screen.width * (5f / 19f), Screen.height * (7.5f / 12f), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
+            else if (cursor == (int)State.Accept)
+                GUI.DrawTexture(new Rect(Screen.width * (5f / 19f), Screen.height * (9.5f / 12f), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
+            else
+                GUI.DrawTexture(new Rect(Screen.width * (9f / 19f), Screen.height * (9.5f / 12f), Screen.width * (1f / 19f), Screen.height * (1f / 12f)), CursorPic);
         }
     }
 }
