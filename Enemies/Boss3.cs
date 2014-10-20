ab4911519302ac338266f16c9b0507c77c7841b3
Jonathan Hunter <hunterjonathant@gmail.com>
2014-10-20 10:04:40 -0400

    Boss3 added

---
+++ Enemies/Boss3.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Enemies
+{
+    class Boss3 : Enemy
+    {
+        public Transform left;
+        public GameObject DownSlashAttack;
+        public GameObject Wave;
+        public Transform LeftSpawn;
+        public Transform RightSpawn;
+        public GameObject SlashAttack;
+        public float close;
+        public float invulerabilityTime;
+        public float speed;
+
+        private Transform player;
+        private static GameObject attack;
+        private int prevState = 0;
+        private float invulerability = 0;
+        private bool doOnce = false;
+        private bool render = true;
+        private bool hitPlayer = false;
+
+
+        protected override EnemyStateMachine getStateMachine(int frameRate)
+        {
+            return new Boss3StateMachine(frameRate);
+        }
+
+        protected override void Initialize()
+        {
+            player = FindObjectOfType<Player.Player>().gameObject.transform;
+        }
+
+        void OnCollisionEnter2D(Collision2D coll)
+        {
+            if (!Data.Paused)
+            {
+                if (coll.gameObject.tag == "Enemy" || coll.gameObject.tag == "Health")
+                    Physics2D.IgnoreCollision(this.gameObject.collider2D, coll.gameObject.collider2D);
+                if (coll.gameObject.tag == "PlayerAttack")
+                    beingHit = true;
+                if (coll.gameObject.tag == "Player")
+                    hitPlayer = true;
+            }
+        }
+
+        protected override bool[] getFlags()
+        {
+            float distx = Mathf.Abs(player.position.x - this.gameObject.transform.position.x);
+            float disty = Mathf.Abs(player.position.y - this.gameObject.transform.position.y);
+            bool playerClose = distx < close && disty < 1;
+            bool inAir = false;
+            bool OnWall = false;
+            TouchingSomething(ref inAir, ref OnWall);
+            return new bool[] { inAir, OnWall,  playerClose, hitPlayer, playerClose};
+        }
+
+        private void TouchingSomething(ref bool inAir, ref bool nextToClimableWall)
+        {
+            inAir = !(Physics2D.Raycast(backFoot.position, -Vector2.up, 0.05f) || Physics2D.Raycast(frontFoot.position, -Vector2.up, 0.05f));
+            RaycastHit2D ray1, ray2;
+            if (this.transform.localScale.x > 0)
+            {
+                ray1 = Physics2D.Raycast(right.position, -Vector2.right, 0.05f);
+                ray2 = Physics2D.Raycast(left.position, Vector2.right, 0.05f);
+            }
+            else
+            {
+                ray1 = Physics2D.Raycast(right.position, Vector2.right, 0.05f);
+                ray2 = Physics2D.Raycast(left.position, -Vector2.right, 0.05f);
+            }
+            if (ray1 == null || ray1.collider == null)
+                nextToClimableWall = false;
+            else
+            {
+                if (ray1.collider.tag == "Ground")
+                    nextToClimableWall = true;
+                else
+                    nextToClimableWall = false;
+            }
+            if (!nextToClimableWall)
+            {
+                if (ray2 == null || ray2.collider == null)
+                    nextToClimableWall = false;
+                else
+                {
+                    if (ray2.collider.tag == "Ground")
+                        nextToClimableWall = true;
+                    else
+                        nextToClimableWall = false;
+                }
+            }
+        }
+
+        protected override void RunBehavior(int state)
+        {
+            if (state != prevState)
+            {
+                doOnce = false;
+                prevState = state;
+                if (attack != null)
+                    Destroy(attack);
+            }
+            if (hitPlayer)
+                hitPlayer = false;
+            switch (state)
+            {
+                case (int)Boss3StateMachine.State.Intro: Intro(); break;
+                case (int)Boss3StateMachine.State.BackFlip: BackFlip(); break;
+                case (int)Boss3StateMachine.State.Transition: Transition(); break;
+                case (int)Boss3StateMachine.State.WallJump: WallJump(); break;
+                case (int)Boss3StateMachine.State.DownSlash: DownSlash(); break;
+                case (int)Boss3StateMachine.State.DownSlashWait: DownSlashWait(); break;
+                case (int)Boss3StateMachine.State.Dash: Dash(); break;
+                case (int)Boss3StateMachine.State.Slash: Slash(); break;
+                case (int)Boss3StateMachine.State.SlashWait: SlashWait(); break;
+            }
+            if (invulerability > 0)
+            {
+                render = !render;
+                renderer.enabled = render;
+                damage = 0;
+                beingHit = false;
+                invulerability -= Time.deltaTime;
+            }
+            else
+                renderer.enabled = true;
+            if (beingHit)
+            {
+                damage = 1;
+                invulerability = invulerabilityTime;
+            }
+        }
+
+        private void Intro()
+        {
+        }
+        private void BackFlip()
+        {
+            transform.Translate(-getForward().x * speed * Time.deltaTime, Vector2.up.y * speed * Time.deltaTime, 0f);
+        }
+        private void Transition()
+        {
+            transform.Translate(-getForward() * speed * Time.deltaTime);
+        }
+        private void WallJump()
+        {
+            transform.Translate(getForward() * speed * Time.deltaTime);
+        }
+        private void DownSlash()
+        {
+            if (!doOnce)
+            {
+                attack = ((GameObject)Instantiate(DownSlashAttack));
+                attack.GetComponent<Player.Attack>().setReference(this.gameObject.transform);
+                doOnce = true;
+            }
+        }
+        private void DownSlashWait()
+        {
+            if (!doOnce)
+            {
+                GameObject temp = (GameObject)Instantiate(Wave);
+                temp.transform.position = LeftSpawn.position;
+                temp.transform.localScale = new Vector3(Mathf.Sign(LeftSpawn.transform.localScale.x) * temp.transform.localScale.x, temp.transform.localScale.y, temp.transform.localScale.z); 
+                temp = (GameObject)Instantiate(Wave);
+                temp.transform.position = RightSpawn.position;
+                temp.transform.localScale = new Vector3(Mathf.Sign(RightSpawn.transform.localScale.x) * temp.transform.localScale.x, temp.transform.localScale.y, temp.transform.localScale.z);
+                doOnce = true;
+            }
+        }
+        private void Dash()
+        {
+            transform.Translate(getForward() * speed * Time.deltaTime);
+        }
+        private void Slash()
+        {
+            if (!doOnce)
+            {
+                attack = ((GameObject)Instantiate(SlashAttack));
+                attack.GetComponent<Player.Attack>().setReference(this.gameObject.transform);
+                doOnce = true;
+            }
+        }
+        private void SlashWait()
+        {
+        }
+    }
+}
---
+++ Enemies/Boss3StateMachine.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Enemies
+{
+    class Boss3StateMachine : EnemyStateMachine
+    {
+        public enum State
+        {
+            Intro = 0, BackFlip, Transition, WallJump, DownSlash, DownSlashWait, Dash, Slash, SlashWait
+        };
+        private double hold;
+
+        public Boss3StateMachine(int frameRate) : base(frameRate) { }
+
+        protected override int[] Initialize()
+        {
+            return new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        }
+
+        protected override int StateMachine(int currState, bool beingHit, bool[] flags)
+        {
+            switch (currState)
+            {
+                case (int)State.Intro: currState = (int)Intro(); break;
+                case (int)State.BackFlip: currState = (int)BackFlip(); break;
+                case (int)State.Transition: currState = (int)Transition(flags[0], flags[1]); break;
+                case (int)State.WallJump: currState = (int)WallJump(flags[1], flags[2], flags[3]); break;
+                case (int)State.DownSlash: currState = (int)DownSlash(flags[0]); break;
+                case (int)State.DownSlashWait: currState = (int)DownSlashWait(); break;
+                case (int)State.Dash: currState = (int)Dash(flags[4]); break;
+                case (int)State.Slash: currState = (int)Slash(); break;
+                case (int)State.SlashWait: currState = (int)SlashWait(); break;
+            }
+            return currState;
+        }
+
+        private State Intro()
+        {
+            if (animHandler.isDone((int)State.Intro))
+                return State.BackFlip;
+            return State.Intro;
+        }
+        private State BackFlip()
+        {
+            if (animHandler.isDone((int)State.BackFlip))
+                return State.Transition;
+            return State.BackFlip;
+        }
+        private State Transition(bool inAir, bool onWall)
+        {
+            if (onWall)
+                return State.WallJump;
+            if (!inAir)
+            {
+                if (UnityEngine.Random.Range(0, 2) == 1)
+                    return State.Dash;
+                return State.BackFlip;
+            }
+            return State.Transition;
+        }
+        private State WallJump(bool OnWall, bool overPlayer, bool hitPlayer)
+        {
+            if (OnWall || overPlayer || hitPlayer)
+                return State.DownSlash;
+            return State.WallJump; 
+        }
+        private State DownSlash(bool inAir)
+        {
+            if (!inAir)
+                return State.DownSlashWait;
+            return State.DownSlash;
+        }
+        private State DownSlashWait()
+        {
+            hold += UnityEngine.Time.deltaTime;
+            if (hold > 1.5f)
+            {
+                hold = 0;
+                return State.Dash;
+            }
+            return State.DownSlashWait;
+        }
+        private State Dash(bool atDestination)
+        {
+            if (atDestination)
+                return State.Slash;
+            return State.Dash;
+        }
+        private State Slash()
+        {
+            if (animHandler.isDone((int)State.Slash))
+                return State.SlashWait;
+            return State.Slash;
+        }
+        private State SlashWait()
+        {
+            hold += UnityEngine.Time.deltaTime;
+            if (hold > 1.5f)
+            {
+                hold = 0;
+                return State.BackFlip;
+            }
+            return State.SlashWait;
+        }
+    }
+}
