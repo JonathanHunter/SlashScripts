0f02ef43d519afe8357bd2ce6bc12c3ec6e8722f
Jonathan Hunter <hunterjonathant@gmail.com>
2014-10-24 16:20:06 -0400

    Main menu animation

---
+++ Menus/TitleScroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Menus
+{
+    class TitleScroller : MonoBehaviour
+    {
+        void Update()
+        {
+            if (this.transform.position.x < -16)
+            {
+                this.transform.position = new Vector3(16,0,0);
+            }
+            this.transform.Translate(-Vector2.right * Time.deltaTime);
+        }
+    }
+}
