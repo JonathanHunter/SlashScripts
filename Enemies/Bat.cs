d0ced637c64c46f9e0486980bb30f0d0cda89e3d
Jonathan Hunter <hunterjonathant@gmail.com>
2014-11-07 13:47:57 -0500

    Bat Boss

---
+++ Enemies/Bat.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Enemies
+{
+    class Bat : Enemy
+    {
+        public Transform[] MovePositions;
+        public float invulerabilityTime;
+        public float flySpeed;
+
+        private Transform player;
+        private int currNode = 0;
+        private float invulerability = 0;
+        private bool render = true;
+
+        protected override EnemyStateMachine getStateMachine(int frameRate)
+        {
+            return new BatStateMachine(frameRate);
+        }
+
+        protected override void Initialize()
+        {
+            player = FindObjectOfType<Player.Player>().gameObject.transform;
+        }
+
+        protected override bool[] getFlags()
+        {
+            return new bool[] { hitPlayer };
+        }
+
+        protected override void RunBehavior(int state)
+        {
+            switch (state)
+            {
+                case (int)BatStateMachine.State.Intro: Intro(); break;
+                case (int)BatStateMachine.State.FlyOnCircuit: Fly(); break;
+                case (int)BatStateMachine.State.FlyToPlayer: Attack(); break;
+            }
+            if (invulerability > 0)
+            {
+                render = !render;
+                renderer.enabled = render;
+                damage = 0;
+                beingHit = false;
+                invulerability -= Time.deltaTime;
+            }
+            else if (!renderer.enabled)
+                renderer.enabled = true;
+            if (beingHit)
+            {
+                damage = 1;
+                invulerability = invulerabilityTime;
+            }
+        }
+
+        public void Intro()
+        {
+        }
+        
+        public void Fly()
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, MovePositions[currNode].position, flySpeed * Time.deltaTime);
+            if ((Mathf.Abs(this.transform.position.x - MovePositions[currNode].position.x) < .01) && (Mathf.Abs(this.transform.position.y - MovePositions[currNode].position.y) < .01))
+            {
+                currNode++;
+                currNode %= MovePositions.Length;
+            }
+        }
+
+        public void Attack()
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, player.position, flySpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Enemies/BatStateMachine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Enemies
+{
+    class BatStateMachine : EnemyStateMachine
+    {
+        public enum State { Intro, FlyOnCircuit, FlyToPlayer};
+
+        private float wait;
+
+        public BatStateMachine(int frameRate) : base(frameRate) { }
+
+        protected override int[] Initialize()
+        {
+            wait = 0;
+            return new int[] { 6, 1, 1};
+        }
+
+        protected override int StateMachine(int currState, bool beingHit, bool[] flags)
+        {
+            switch(currState)
+            {
+                case (int)State.Intro: return Intro(); break;
+                case (int)State.FlyOnCircuit: return FLY(); break;
+                case (int)State.FlyToPlayer: return ATTACK(beingHit, flags[0]); break;
+            }
+            return 0;
+        }
+
+        public int Intro()
+        {
+            if (animHandler.isDone((int)State.Intro))
+                return (int)State.FlyOnCircuit;
+            return (int)State.Intro;
+        }
+
+        public int FLY()
+        {
+            wait+=UnityEngine.Time.deltaTime;
+            if(wait>5f)
+            {
+                wait=0;
+                return (int)State.FlyToPlayer;
+            }
+            return (int)State.FlyOnCircuit;
+        }
+
+        public int ATTACK(bool hurt, bool hit)
+        {
+            if (hit || hurt)
+                return (int)State.FlyOnCircuit;
+            return (int)State.FlyToPlayer;
+        }
+    }
+}
