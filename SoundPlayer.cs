64ee531444b462152665d8bf44875faf9f3d7a2c
Jonathan Hunter <hunterjonathant@gmail.com>
2014-11-07 13:49:23 -0500

    Intro support

--- SoundPlayer.cs
+++ SoundPlayer.cs
@@ -9,15 +9,25 @@ namespace Assets.Scripts
         public bool SFX;
         public bool playOnLoad;
         public bool loop;
+        public bool intro;
+        public int loopSong;
+        public bool dontDestroy;
 
         void Start()
         {
             if (playOnLoad)
                 PlaySong(0);
+            if (dontDestroy)
+                DontDestroyOnLoad(this.gameObject);
         }
 
         void Update()
         {
+            if (intro && !audio.isPlaying)
+            {
+                intro = false;
+                PlaySong(1);
+            }
         }
 
         public void PlaySong(int index)
@@ -27,7 +37,7 @@ namespace Assets.Scripts
                 audio.volume = Data.SfxVol;
             else
                 audio.volume = Data.MusicVol;
-            audio.loop = loop;
+            audio.loop = loop&&index==loopSong;
             audio.clip = song[index];
             audio.Play();
         }
