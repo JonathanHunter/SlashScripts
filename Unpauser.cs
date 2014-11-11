1d1ef6c247e298760f9c29608c8c361c50a8ac2c
Jonathan Hunter <hunterjonathant@gmail.com>
2014-11-11 11:07:51 -0500

    prevent pause on level start

---
+++ Unpauser.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    class Unpauser : MonoBehaviour
+    {
+        void Start()
+        {
+            Data.Paused = false;
+            Destroy(this);
+        }
+    }
+}
